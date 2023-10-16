76722e5647d4b7fdd28fcf71d1340d770d3212c1
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-10-16 08:35:25 +0530

    Implement move validation logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -21,11 +21,19 @@ const ticTacToe = (xName, oName) => {
         O: X
     };
 
+    const isValidMove = (move) => {
+        return (1 <= move && move <= 9) && board[move] === "";
+    }
+
     return (player, move) => {
         // validate right player
         if (player !== currentPlayer) {
             return [false, `Not your turn. It's ${currentPlayer}'s turn.`];
         }
+
+        // validate right move
+        if(!isValidMove(move))
+            return [false, "Invalid move, try again."];
     };
 };
 
